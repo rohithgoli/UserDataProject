efcc39125ebba826858f9663d9f09dd34c44cc5f
rohith_goli <rohithgoli396@gmail.com>
2021-05-24 11:24:33 +0530

    basic styling and created branch for sql

--- userData.py
+++ userData.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for,jsonify
+from flask import Flask, render_template, request, redirect, url_for, jsonify
 import csv
 
 app = Flask(__name__)
