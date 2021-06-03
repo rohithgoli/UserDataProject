11ee01b837e91b8e25c210c64d03b4e577d6ede2
rohith_goli <rohithgoli396@gmail.com>
2021-06-03 18:21:55 +0530

    modified sql with standard practice

--- userData.py
+++ userData.py
@@ -11,9 +11,9 @@ def user_choice():
     return render_template('userChoice.html')
 
 
-@app.route('/choice', methods = ['POST','GET'])
+@app.route('/choice', methods=['POST'])
 def do_user_choice():
-    if request.method == 'POST':
+    try:
         choice = request.form['choice']
         if choice == 'add':
             return redirect(url_for('add_user_data'))
@@ -21,6 +21,11 @@ def do_user_choice():
             return redirect(url_for('search_user_data'))
         elif choice == 'view':
             return redirect(url_for('display_data'))
+        else:
+            raise ValueError
+    except ValueError:
+        print("Invalid choice :(")
+
 
 # User Adding Data API
 
@@ -30,29 +35,27 @@ def add_user_data():
     return render_template('userAddData.html')
 
 
-@app.route('/add-data', methods = ['POST','GET'])
+@app.route('/add-data', methods=['POST'])
 def add_data():
-    if request.method == 'POST':
-      try:
-        Name = request.form['Name']
-        Age = request.form['Age']
-        Gender = request.form['Gender']
-        email = request.form['email']
+    try:
+        name = request.form.get('Name')
+        age = request.form.get('Age')
+        gender = request.form.get('Gender')
+        email = request.form.get('email')
         with sqlite3.connect('users.db') as con:
             cursor = con.cursor()
             cursor.execute("INSERT INTO users(Name,Age,Gender,email)\
-                            VALUES (?,?,?,?)",(Name,Age,Gender,email))
+                            VALUES (?,?,?,?)", (name, age, gender, email))
             con.commit()
-
         msg = "Data Added successfully!! :)"
 
-      except:
+    except():
         con.rollback()
         msg = ":( Error in Adding Data !!"
 
-      finally:
-          con.close()
-          return f"{msg}"
+    finally:
+        con.close()
+        return f"{msg}"
 
 # User Searching Data API
 
@@ -67,35 +70,34 @@ def display_no_results():
     return '<html><body><h1>No Results Found!!</h1></body></html>'
 
 
-@app.route('/search-data', methods = ['POST', 'GET'])
+@app.route('/search-data', methods=['POST'])
 def search_data():
     if request.method == 'POST':
         try:
             column = request.form['column']
-            searchValue = request.form['searchValue']
+            search_value = request.form['searchValue']
             result_dict = {
-            "desiredResult": [],
-            "count": 0
+                "desiredResult": [],
+                "count": 0
             }
 
             with sqlite3.connect('users.db') as con:
                 con.row_factory = sqlite3.Row
                 cursor = con.cursor()
-                cursor.execute(f"SELECT * FROM users WHERE {column} LIKE '%{searchValue}%' ")
+                cursor.execute(f"SELECT * FROM users WHERE {column} LIKE '%{search_value}%' ")
                 rows = cursor.fetchall()
                 for each_row in rows:
                     result_dict['count'] += 1
                     user_dict = {}
-                    columnNames = each_row.keys()
-                    for key in columnNames:
+                    column_names = each_row.keys()
+                    for key in column_names:
                         user_dict[key] = each_row[key]
                     result_dict['desiredResult'].append(user_dict)
 
             if result_dict['count'] == 0:
                 return redirect(url_for('display_no_results'))
             else:
-                # return jsonify(result_dict)
-                return render_template('displayResult.html', result=result_dict, colNames=columnNames)
+                return render_template('displayResult.html', result=result_dict, colNames=column_names)
         except:
             con.rollback()
         finally:
@@ -105,33 +107,32 @@ def search_data():
 # Displaying User Data API
 
 
-@app.route('/display', methods = ['GET'])
+@app.route('/display', methods=['GET'])
 def display_data():
-    if request.method == 'GET':
-        try:
-            result_dict = {
-                'count':0,
-                'desiredResult':[]
-                }
-            with sqlite3.connect('users.db') as con:
-                con.row_factory = sqlite3.Row
-                cursor = con.cursor()
-                cursor.execute("SELECT * FROM users;")
-                rows = cursor.fetchall()
+    try:
+        result_dict = {
+            'count': 0,
+            'desiredResult': []
+            }
+        with sqlite3.connect('users.db') as con:
+            con.row_factory = sqlite3.Row
+            cursor = con.cursor()
+            cursor.execute("SELECT * FROM users;")
+            rows = cursor.fetchall()
 
-                for row in rows:
-                    result_dict['count'] += 1
-                    user_dict = {}
-                    columnNames = row.keys()
-                    for key in columnNames:
-                        user_dict[key] = row[key]
-                    result_dict['desiredResult'].append(user_dict)
-            return render_template('displayResult.html', result=result_dict, colNames=columnNames)
-        except:
-            con.rollback()
-        finally:
-            con.close()
+            for row in rows:
+                result_dict['count'] += 1
+                user_dict = {}
+                column_names = row.keys()
+                for key in column_names:
+                    user_dict[key] = row[key]
+                result_dict['desiredResult'].append(user_dict)
+        return render_template('displayResult.html', result=result_dict, colNames=column_names)
+    except:
+        con.rollback()
+    finally:
+        con.close()
 
 
 if __name__ == '__main__':
-    app.run(debug = True)
+    app.run(debug=True)
